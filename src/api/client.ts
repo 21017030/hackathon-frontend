4da515e4dca1347a2ea021a91b7405ec9fba244c
21017030 <21017030@suwon.ac.kr>
2026-05-14 00:40:50 +0900

    feat: 백엔드 URL 환경변수 처리

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// 백엔드 API 기본 설정 (baseURL: 로컬 개발 서버)
 const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8000',
 });
 
 export default client;
